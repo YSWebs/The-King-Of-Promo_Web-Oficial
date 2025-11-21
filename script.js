9ef84cf59e98b34cb36ba2279e7dffa396da8828
YSWebs <ysmwebs@gmail.com>
2025-11-21 06:27:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 		function search(){
 			var num_cols, display, input, filter, table_body, tr, td, i, txtValue;
-			num_cols = 2;
+			num_cols = 1;
 			input = document.getElementById("q");
 			filter = input.value.toUpperCase();
 			table_body = document.getElementById("the_table_body");
@@ -23,11 +23,41 @@
 		}		
 	
 function cerrardivvideo(){
+	contentdiv.style.display="block";
 	document.getElementById("divflotvideoview").style.display="none";
 document.getElementById("videoreproendiv").pause();
 }
-function openvideochrislebronprestame(){
-	setTimeout(() => {videoreproendiv.play();}, 1000);
+function openvideodiv(){
+	contentdiv.style.display="none";
+	setTimeout(() => {videoreproendiv.play();}, 100);
 	document.getElementById("divflotvideoview").style.display="block";
-
 }
+function pausarTodosLosVideos() {
+	setTimeout(() => {
+
+    const videos = document.querySelectorAll('.videoiddivcontenttable'); // Selecciona todos los videos
+    videos.forEach(video => {
+        video.pause(); // Pausa cada video
+        video.currentTime = 0; // Opcional: reinicia el video al inicio
+    });
+		}, 1000);
+}
+
+// Llama a la función cuando se hace clic en el botón
+document.getElementById('pausarButton').addEventListener('click', pausarTodosLosVideos);
+
+
+function openvideochrislebronprestame(){
+	videoreproendiv.src="Chris_Lebron_-_Préstame__Video_Lyrics____En_Honor_Al_Que_Ama(144p).mp4";
+videotitle.innerHTML="Chris Lebron - Prestame";
+	videoinfo.innerHTML="Cantante: Chris Lebron <br> Tema: Prestame";
+	setTimeout(() => {openvideodiv();}, 100);
+}
+function openvideofiesta1(){
+	videoreproendiv.src="VID-20251118-WA0000.mp4";
+videotitle.innerHTML="fiesta 1";
+	videoinfo.innerHTML="Descripcion fiesta 1";
+	setTimeout(() => {openvideodiv();}, 100);
+}
+
+
