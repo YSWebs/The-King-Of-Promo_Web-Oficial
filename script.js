2b0387d7bcd2e0f1a0776712bd00d523486eae8d
YSWebs <ysmwebs@gmail.com>
2025-11-24 14:17:52 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-
-
+        
 		function search(){
 			var num_cols, display, input, filter, table_body, tr, td, i, txtValue;
 			num_cols = 1;
@@ -28,6 +27,10 @@ function cerrardivvideo(){
 	contentdiv.style.display="block";
 	document.getElementById("divflotvideoview").style.display="none";
 document.getElementById("videoreproendiv").pause();
+	videoreproendiv.style.display="none";
+	img1divflotvideo.style.display="none"
+	videotitle.innerHTML="Titulo";
+	videoinfo.innerHTML="Descripción";
 }
 function openvideodiv(){
 	contentdiv.style.display="none";
@@ -37,22 +40,14 @@ function openvideodiv(){
 
 
 
-function openvideochrislebronprestame(){
-	videoreproendiv.src="Chris_Lebron_-_Préstame__Video_Lyrics____En_Honor_Al_Que_Ama(144p).mp4";
-videotitle.innerHTML="Chris Lebron - Prestame";
-	videoinfo.innerHTML="Cantante: Chris Lebron <br> Tema: Prestame";
-	setTimeout(() => {openvideodiv();}, 100);
-}
+
 function openvideofiesta1(){
 	videoreproendiv.src="VID-20251118-WA0000.mp4";
+	videoreproendiv.style.display="block";
+	img1divflotvideo.style.display="block";
 videotitle.innerHTML="fiesta 1";
 	videoinfo.innerHTML="Descripcion fiesta 1";
 	setTimeout(() => {openvideodiv();}, 100);
 }
-function openvideochamasvailando(){
-	videoreproendiv.src="VID-20251118-WA0001.mp4";
-videotitle.innerHTML="chamasvailando";
-	videoinfo.innerHTML="Descripcion vaile chamas";
-	setTimeout(() => {openvideodiv();}, 100);
-}
+
 
