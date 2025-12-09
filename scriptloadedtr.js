720201e7e03ab03f7e92ea0173394e12ad0a937c
YSWebs <ysmwebs@gmail.com>
2025-12-09 09:36:07 -0500

    Add files via upload

--- scriptloadedtr.js
+++ scriptloadedtr.js
@@ -5,4 +5,9 @@ window.onload = function() {
 }, 100);
     setTimeout(() => {
    trloadtabletext.style.display="none"}, 1300);
+setTimeout(() => {
+   if(trloadtabletext.innerHTML=="Cargando..."){
+      trloadtabletext.innerHTML="Cargando...<br>Su conexion a Internet es lenta o inestable.<br>Por favor revise su conexion a internet."
+   }
+}, 13000);
 }
