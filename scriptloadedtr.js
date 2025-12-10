30b3428afac8de37d5aafe97fa0fa89007ba1f30
YSWebs <ysmwebs@gmail.com>
2025-12-10 13:01:33 -0500

    Add files via upload

--- scriptloadedtr.js
+++ scriptloadedtr.js
@@ -5,6 +5,7 @@ window.onload = function() {
 }, 100);
     setTimeout(() => {
    trloadtabletext.style.display="none"}, 1300);
+   botonverpubdown1.style.display="block"
 setTimeout(() => {
    if(trloadtabletext.style.display=="table"){
       tdload.innerHTML="Cargando...<br>Su conexion a Internet es lenta o inestable.<br>Por favor revise su conexion a internet."
